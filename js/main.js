4dd1f1ab7cc024f0195b6429a6dd66bba2477307
maybou <uminokaze521@gmail.com>
2017-11-11 15:54:31 +0900

    get time

---
+++ js/main.js
@@ -0,0 +1,19 @@
+(function(){
+  'use strict';
+  var timer = document.getElementById('time-text');
+  var start = document.getElementById('start');
+  var stop = document.getElementById('stop');
+  var reset = document.getElementById('reset');
+
+  function show(){
+    var date_obj = new Date();
+
+    var milliseconds = date_obj.getTime();
+
+    // console.log(milliseconds);
+    setTimeout(function(){
+        show();
+    },10);
+  }
+  show();
+})();
