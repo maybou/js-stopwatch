8c1c785dcb3dce3d7f8806e1d42b6124ec963fe8
maybou <uminokaze521@gmail.com>
2017-11-11 16:45:32 +0900

    feature start button

--- js/main.js
+++ js/main.js
@@ -4,16 +4,32 @@
   var start = document.getElementById('start');
   var stop = document.getElementById('stop');
   var reset = document.getElementById('reset');
+  var start_time;
+
+  start.addEventListener('click',function(){
+    var date_obj = new Date();
+    start_time = date_obj.getTime();
+    show();
+  });
 
   function show(){
     var date_obj = new Date();
-
-    var milliseconds = date_obj.getTime();
-    timer.textContent = milliseconds;
-    // console.log(milliseconds);
+    var current_time = date_obj.getTime();
+    var elapsed_time = current_time - start_time;
+    timer.textContent = convertTimerText(elapsed_time);
     setTimeout(function(){
         show();
     },10);
   }
-  show();
+  // timer.textContent = convertTimerText(654321);
+  function convertTimerText(elapsed_time){
+    var min = Math.floor(elapsed_time / 60000);
+    var sec = Math.floor(elapsed_time % 60000 / 1000);
+    var msec = elapsed_time % 1000;
+    min = ('0' + min).slice(-2);
+    sec = ('0' + sec).slice(-2);
+    msec = ('00' + msec).slice(-3);
+    return min + ":" + sec + ":" + msec;
+  }
+
 })();
